110081b62787fb063ef50731784129cf56036fc9
Administrator <Administrator@172.31.31.243>
2017-03-09 09:58:55 +0000

    Commiting index.html and manifest.v3

--- code/source/PSUtilities/hashtable.java
+++ code/source/PSUtilities/hashtable.java
@@ -1,8 +1,8 @@
 package PSUtilities;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2005-06-10 15:24:27 PDT
-// -----( ON-HOST: 172.20.6.126
+// -----( CREATED: 2017-03-09 09:42:41 UTC
+// -----( ON-HOST: 172.31.31.243
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -10,234 +10,257 @@ import com.wm.app.b2b.server.Service;
 import com.wm.app.b2b.server.ServiceException;
 // --- <<IS-START-IMPORTS>> ---
 import java.util.*;
-
 // --- <<IS-END-IMPORTS>> ---
 
 public final class hashtable
 
 {
-  // ---( internal utility methods )---
+	// ---( internal utility methods )---
 
-  final static hashtable _instance = new hashtable();
+	final static hashtable _instance = new hashtable();
 
-  static hashtable _newInstance()
-  {
-    return new hashtable();
-  }
+	static hashtable _newInstance() { return new hashtable(); }
 
-  static hashtable _cast(Object o)
-  {
-    return (hashtable) o;
-  }
+	static hashtable _cast(Object o) { return (hashtable)o; }
 
-  // ---( server methods )---
+	// ---( server methods )---
 
-  public static final void containsKey(IData pipeline) throws ServiceException
-  {
-    // --- <<IS-START(containsKey)>> ---
-    // @subtype unknown
-    // @sigtype java 3.5
-    // [i] object:0:required Hashtable
-    // [i] field:0:required key
-    // [o] field:0:required containsKey {"true","false"}
 
-    IDataCursor idcPipeline = pipeline.getCursor();
 
-    Hashtable ht;
-    if (idcPipeline.first("Hashtable"))
-    {
-      ht = (Hashtable) idcPipeline.getValue();
-    }
-    else
-    {
-      throw new ServiceException("Hashtable is null!");
-    }
 
-    idcPipeline.first("key");
-    String strKey = (String) idcPipeline.getValue();
+	public static final void containsKey (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(containsKey)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [i] object:0:required Hashtable
+		// [i] field:0:required key
+		// [o] field:0:required containsKey {"true","false"}
+		
+		IDataCursor idcPipeline = pipeline.getCursor();
+		
+		Hashtable ht;
+		if (idcPipeline.first("Hashtable"))
+		{
+		  ht = (Hashtable) idcPipeline.getValue();
+		}
+		else
+		{
+		  throw new ServiceException("Hashtable is null!");
+		}
+		
+		idcPipeline.first("key");
+		String strKey = (String) idcPipeline.getValue();
+		
+		idcPipeline.insertAfter("containsKey", String.valueOf(ht.containsKey(strKey)));
+		
+		idcPipeline.destroy();
+		// --- <<IS-END>> ---
 
-    idcPipeline.insertAfter("containsKey", String.valueOf(ht.containsKey(strKey)));
+                
+	}
 
-    idcPipeline.destroy();
-    // --- <<IS-END>> ---
 
-  }
 
-  public static final void createHashtable(IData pipeline) throws ServiceException
-  {
-    // --- <<IS-START(createHashtable)>> ---
-    // @subtype unknown
-    // @sigtype java 3.5
-    // [o] object:0:required Hashtable
+	public static final void createHashtable (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(createHashtable)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [o] object:0:required Hashtable
+		
+		IDataCursor idcPipeline = pipeline.getCursor();
+		idcPipeline.insertAfter("Hashtable", new Hashtable());
+		idcPipeline.destroy();
+		// --- <<IS-END>> ---
 
-    IDataCursor idcPipeline = pipeline.getCursor();
-    idcPipeline.insertAfter("Hashtable", new Hashtable());
-    idcPipeline.destroy();
-    // --- <<IS-END>> ---
+                
+	}
 
-  }
 
-  public static final void get(IData pipeline) throws ServiceException
-  {
-    // --- <<IS-START(get)>> ---
-    // @subtype unknown
-    // @sigtype java 3.5
-    // [i] object:0:required Hashtable
-    // [i] field:0:required key
-    // [o] object:0:required value
 
-    IDataCursor idcPipeline = pipeline.getCursor();
+	public static final void get (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(get)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [i] object:0:required Hashtable
+		// [i] field:0:required key
+		// [o] object:0:required value
+		
+		IDataCursor idcPipeline = pipeline.getCursor();
+		
+		Hashtable ht;
+		if (idcPipeline.first("Hashtable"))
+		{
+		  ht = (Hashtable) idcPipeline.getValue();
+		}
+		else
+		{
+		  throw new ServiceException("Hashtable is null!");
+		}
+		
+		idcPipeline.first("key");
+		String strKey = (String) idcPipeline.getValue();
+		
+		idcPipeline.insertAfter("value", ht.get(strKey));
+		
+		idcPipeline.destroy();
+		// --- <<IS-END>> ---
 
-    Hashtable ht;
-    if (idcPipeline.first("Hashtable"))
-    {
-      ht = (Hashtable) idcPipeline.getValue();
-    }
-    else
-    {
-      throw new ServiceException("Hashtable is null!");
-    }
+                
+	}
 
-    idcPipeline.first("key");
-    String strKey = (String) idcPipeline.getValue();
 
-    idcPipeline.insertAfter("value", ht.get(strKey));
 
-    idcPipeline.destroy();
-    // --- <<IS-END>> ---
+	public static final void keys (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(keys)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [i] object:0:required Hashtable
+		// [o] field:1:required keys
+		
+		IDataCursor idcPipeline = pipeline.getCursor();
+		
+		Hashtable ht;
+		if (idcPipeline.first("Hashtable"))
+		{
+		  ht = (Hashtable) idcPipeline.getValue();
+		}
+		else
+		{
+		  throw new ServiceException("Hashtable is null!");
+		}
+		
+		idcPipeline.insertAfter("keys", ht.keySet().toArray());
+		
+		idcPipeline.destroy();
+		// --- <<IS-END>> ---
 
-  }
+                
+	}
 
-  public static final void keys(IData pipeline) throws ServiceException
-  {
-    // --- <<IS-START(keys)>> ---
-    // @subtype unknown
-    // @sigtype java 3.5
-    // [i] object:0:required Hashtable
-    // [o] field:1:required keys
 
-    IDataCursor idcPipeline = pipeline.getCursor();
 
-    Hashtable ht;
-    if (idcPipeline.first("Hashtable"))
-    {
-      ht = (Hashtable) idcPipeline.getValue();
-    }
-    else
-    {
-      throw new ServiceException("Hashtable is null!");
-    }
+	public static final void put (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(put)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [i] object:0:required Hashtable
+		// [i] field:0:required key
+		// [i] object:0:required value
+		// [o] object:0:required Hashtable
+		
+		IDataCursor idcPipeline = pipeline.getCursor();
+		
+		Hashtable ht;
+		if (idcPipeline.first("Hashtable"))
+		{
+		  ht = (Hashtable) idcPipeline.getValue();
+		  idcPipeline.delete();
+		}
+		else
+		{
+		  ht = new Hashtable();
+		}
+		
+		String strKey = null;
+		Object objValue = new Object();
+		if (idcPipeline.first("key"))
+		{
+		  strKey = (String) idcPipeline.getValue();
+		}
+		if (idcPipeline.first("value"))
+		{
+		  objValue = idcPipeline.getValue();
+		}
+		
+		ht.put(strKey, objValue);
+		
+		idcPipeline.insertAfter("Hashtable", ht);
+		
+		idcPipeline.destroy();
+		// --- <<IS-END>> ---
 
-    idcPipeline.insertAfter("keys", ht.keySet().toArray());
+                
+	}
 
-    idcPipeline.destroy();
-    // --- <<IS-END>> ---
 
-  }
 
-  public static final void put(IData pipeline) throws ServiceException
-  {
-    // --- <<IS-START(put)>> ---
-    // @subtype unknown
-    // @sigtype java 3.5
-    // [i] object:0:required Hashtable
-    // [i] field:0:required key
-    // [i] object:0:required value
-    // [o] object:0:required Hashtable
+	public static final void remove (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(remove)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [i] object:0:required Hashtable
+		// [i] field:0:required key
+		// [o] object:0:required Hashtable
+		// [o] object:0:required value
+		
+		IDataCursor idcPipeline = pipeline.getCursor();
+		
+		Hashtable ht;
+		if (idcPipeline.first("Hashtable"))
+		{
+		  ht = (Hashtable) idcPipeline.getValue();
+		  idcPipeline.delete();
+		}
+		else
+		{
+		  throw new ServiceException("Hashtable is null!");
+		}
+		
+		idcPipeline.first("key");
+		String strKey = (String) idcPipeline.getValue();
+		
+		Object value = ht.remove(strKey);
+		
+		idcPipeline.insertAfter("Hashtable", ht);
+		idcPipeline.insertAfter("value", value);
+		
+		idcPipeline.destroy();
+		// --- <<IS-END>> ---
 
-    IDataCursor idcPipeline = pipeline.getCursor();
+                
+	}
 
-    Hashtable ht;
-    if (idcPipeline.first("Hashtable"))
-    {
-      ht = (Hashtable) idcPipeline.getValue();
-      idcPipeline.delete();
-    }
-    else
-    {
-      ht = new Hashtable();
-    }
 
-    String strKey = null;
-    Object objValue = new Object();
-    if (idcPipeline.first("key"))
-    {
-      strKey = (String) idcPipeline.getValue();
-    }
-    if (idcPipeline.first("value"))
-    {
-      objValue = idcPipeline.getValue();
-    }
 
-    ht.put(strKey, objValue);
+	public static final void size (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(size)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [i] object:0:required Hashtable
+		// [o] field:0:required size
+		
+		IDataCursor idcPipeline = pipeline.getCursor();
+		
+		Hashtable ht;
+		if (idcPipeline.first("Hashtable"))
+		{
+		  ht = (Hashtable) idcPipeline.getValue();
+		}
+		else
+		{
+		  throw new ServiceException("Hashtable is null!");
+		}
+		
+		idcPipeline.insertAfter("size", String.valueOf(ht.size()));
+		
+		idcPipeline.destroy();
+		// --- <<IS-END>> ---
 
-    idcPipeline.insertAfter("Hashtable", ht);
-
-    idcPipeline.destroy();
-    // --- <<IS-END>> ---
-
-  }
-
-  public static final void remove(IData pipeline) throws ServiceException
-  {
-    // --- <<IS-START(remove)>> ---
-    // @subtype unknown
-    // @sigtype java 3.5
-    // [i] object:0:required Hashtable
-    // [i] field:0:required key
-    // [o] object:0:required Hashtable
-    // [o] object:0:required value
-
-    IDataCursor idcPipeline = pipeline.getCursor();
-
-    Hashtable ht;
-    if (idcPipeline.first("Hashtable"))
-    {
-      ht = (Hashtable) idcPipeline.getValue();
-      idcPipeline.delete();
-    }
-    else
-    {
-      throw new ServiceException("Hashtable is null!");
-    }
-
-    idcPipeline.first("key");
-    String strKey = (String) idcPipeline.getValue();
-
-    Object value = ht.remove(strKey);
-
-    idcPipeline.insertAfter("Hashtable", ht);
-    idcPipeline.insertAfter("value", value);
-
-    idcPipeline.destroy();
-    // --- <<IS-END>> ---
-
-  }
-
-  public static final void size(IData pipeline) throws ServiceException
-  {
-    // --- <<IS-START(size)>> ---
-    // @subtype unknown
-    // @sigtype java 3.5
-    // [i] object:0:required Hashtable
-    // [o] field:0:required size
-
-    IDataCursor idcPipeline = pipeline.getCursor();
-
-    Hashtable ht;
-    if (idcPipeline.first("Hashtable"))
-    {
-      ht = (Hashtable) idcPipeline.getValue();
-    }
-    else
-    {
-      throw new ServiceException("Hashtable is null!");
-    }
-
-    idcPipeline.insertAfter("size", String.valueOf(ht.size()));
-
-    idcPipeline.destroy();
-    // --- <<IS-END>> ---
-
-  }
+                
+	}
 }
+
--- code/source/PSUtilities/object.java
+++ code/source/PSUtilities/object.java
@@ -1,68 +1,72 @@
 package PSUtilities;
 
-// -----( B2B Java Code Template v1.2
-// -----( CREATED: Mon Jun 10 22:38:02 PDT 2002
-// -----( ON-HOST: ESU
+// -----( IS Java Code Template v1.2
+// -----( CREATED: 2017-03-09 09:42:41 UTC
+// -----( ON-HOST: 172.31.31.243
 
 import com.wm.data.*;
 import com.wm.util.Values;
 import com.wm.app.b2b.server.Service;
 import com.wm.app.b2b.server.ServiceException;
-
-// --- <<B2B-START-IMPORTS>> ---
-// --- <<B2B-END-IMPORTS>> ---
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
 
 public final class object
+
 {
-  // ---( internal utility methods )---
+	// ---( internal utility methods )---
 
-  final static object _instance = new object();
+	final static object _instance = new object();
 
-  static object _newInstance()
-  {
-    return new object();
-  }
+	static object _newInstance() { return new object(); }
 
-  static object _cast(Object o)
-  {
-    return (object) o;
-  }
+	static object _cast(Object o) { return (object)o; }
 
-  // ---( server methods )---
+	// ---( server methods )---
 
-  public static final void className(IData pipeline) throws ServiceException
-  {
-    // --- <<B2B-START(className)>> ---
-    // @sigtype java 3.5
-    // [i] object:0:required object
-    // [o] field:0:required name
 
-    // pipeline
-    IDataCursor pipelineCursor = pipeline.getCursor();
-    Object object = IDataUtil.get(pipelineCursor, "object");
 
-    pipelineCursor.insertAfter("name", object.getClass().getName());
-    pipelineCursor.destroy();
-    // --- <<B2B-END>> ---
 
-  }
+	public static final void className (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(className)>> ---
+		// @sigtype java 3.5
+		// [i] object:0:required object
+		// [o] field:0:required name
+		
+		// pipeline
+		IDataCursor pipelineCursor = pipeline.getCursor();
+		Object object = IDataUtil.get(pipelineCursor, "object");
+		
+		pipelineCursor.insertAfter("name", object.getClass().getName());
+		pipelineCursor.destroy();
+		// --- <<IS-END>> ---
 
-  public static final void instanceOf(IData pipeline) throws ServiceException
-  {
-    // --- <<B2B-START(instanceOf)>> ---
-    // @sigtype java 3.5
-    // [i] field:0:required class
-    // [i] object:0:required object
-    // [o] field:0:required instanceof
+                
+	}
 
-    // pipeline
-    IDataCursor pipelineCursor = pipeline.getCursor();
-    String class_1 = IDataUtil.getString(pipelineCursor, "class");
-    Object object = IDataUtil.get(pipelineCursor, "object");
 
-    pipelineCursor.insertAfter("instanceof", "" + (object.getClass().getName().equals(class_1)));
-    pipelineCursor.destroy();
-    // --- <<B2B-END>> ---
 
-  }
+	public static final void instanceOf (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(instanceOf)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required class
+		// [i] object:0:required object
+		// [o] field:0:required instanceof
+		
+		// pipeline
+		IDataCursor pipelineCursor = pipeline.getCursor();
+		String class_1 = IDataUtil.getString(pipelineCursor, "class");
+		Object object = IDataUtil.get(pipelineCursor, "object");
+		
+		pipelineCursor.insertAfter("instanceof", "" + (object.getClass().getName().equals(class_1)));
+		pipelineCursor.destroy();
+		// --- <<IS-END>> ---
+
+                
+	}
 }
+
